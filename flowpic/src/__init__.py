37acfc10667534e63b0a0aa84e6d27579e9fd560
Spring-of-Vision <128614411+Spring-of-Vision@users.noreply.github.com>
2024-07-25 18:55:34 +0300

    added flowpic directory
    augmented flowpic project, without dataset

---
+++ flowpic/src/__init__.py
@@ -0,0 +1,9 @@
+import src.data
+import src.features
+import src.models
+import src.charts
+import src.utils
+
+# This is used in the FlowPic code, data loading, and model training/predicting.
+#INPUT_SHAPE = (1500, 1500, 1)
+INPUT_SHAPE = ((1500*1500),)
