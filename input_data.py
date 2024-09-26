be52b4ad556ea1a1b71bd6f941c90909451b06d7
Spring-of-Vision <128614411+Spring-of-Vision@users.noreply.github.com>
2024-09-26 22:58:08 +0300

    added credit

--- input_data.py
+++ input_data.py
@@ -1,3 +1,11 @@
+# Code below was copied and adapted from the original code for the Trustee examples
+# Available at https://github.com/TrusteeML/emperor/tree/main/use_cases/vpn_case
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file, You
+# can obtain one at http://mozilla.org/MPL/2.0/.
+# ==============================================================================
+
 """Functions for downloading and reading MNIST data."""
 from __future__ import absolute_import, division, print_function
 
